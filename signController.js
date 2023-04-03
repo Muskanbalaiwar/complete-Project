0357b35e6a6ed386e8fc08a20c37e007ff84b843
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-04-03 14:44:29 +0530

    Add files via upload

--- signController.js
+++ signController.js
@@ -10,6 +10,34 @@ exports.postData=async(req,res,next)=>{
     const data=await User.create({name:name,email:email,password:password})
     res.status(201).json({details:data});}
     catch(err){
-        console.log(err);
+        res.status(500).json({error:err})
     }
+}
+
+
+exports.login=async(req,res,next)=>{
+  try{
+  const email=req.body._email;
+  const password=req.body._password
+  console.log(password);
+console.log(email);
+ const user= await User.findAll({where:{email}
+  })
+  console.log('user');
+  if(user.length>0){
+    if(user[0].password===password){
+      res.status(200).json({success:true,message:'login seccessfully'})
+    }
+    else{
+      res.status(400).json({success:false,message:'password is incorrect'})
+    }
+  }
+  else{
+    res.status(404).json({success:false,message:'User not exist'})
+  }
+}
+  catch(err){
+   
+   res.status(500).json({message:err,success:false})
+}
 }
--- signRoute.js
+++ signRoute.js
@@ -5,7 +5,7 @@ const adminController = require('../controllers/signController');
 const router = express.Router();
 
 router.post('/sign/post',adminController.postData)
-
+router.post('/login/post',adminController.login)
 
 
 
